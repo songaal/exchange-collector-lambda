9ab70e982564ad14d487d5a9d74b68ac6cb7fb2f
joonwoo88 <jwkim@gncloud.kr>
2018-05-09 10:47:58 +0900

    exchange.has.fetchOhlcv check

--- fetch-exchange-ohlcv/exchange-scheduler.js
+++ fetch-exchange-ohlcv/exchange-scheduler.js
@@ -10,25 +10,22 @@ exports.handler = (event, context, callback) => {
       let exchange_id = process.env.EXCHANGE
       let exchange = new (ccxt)[exchange_id] ()
       let markets = await exchange.load_markets ()
-      // 거래소의 하나의 심볼을 추출하여 1분봉 조회 테스트
-      checkSymbol = Object.keys(markets)[0]
-      console.log('exchange check 1m >> exchange: ', exchange_id, ', symbol: ', checkSymbol)
-      exchange.fetchOHLCV (checkSymbol, '1m')
-      
-      for (var key in markets) {
-          market = markets[key]
-          let attr = {
-              base: market.info.quoteAsset,
-              coin: market.info.baseAsset,
-              symbol: market.symbol,
-              exchange: exchange_id
-          };
-          lambda.invoke({
-              FunctionName: 'exchange-collector',
-              Payload: JSON.stringify(attr)
-          }, function(err, data) {
-              if (err) console.log("err: ", attr.base, attr.coin, err, data);
-          });
+      if (exchange.has.fetchOHLCV == true){
+        for (var key in markets) {
+            market = markets[key]
+            let attr = {
+                base: market.info.quoteAsset,
+                coin: market.info.baseAsset,
+                symbol: market.symbol,
+                exchange: exchange_id
+            };
+            lambda.invoke({
+                FunctionName: 'exchange-collector',
+                Payload: JSON.stringify(attr)
+            }, function(err, data) {
+                if (err) console.log("err: ", attr.base, attr.coin, err, data);
+            });
+        }
       }
   })()
 }
