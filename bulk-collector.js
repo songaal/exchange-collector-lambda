aaed85587efc3f27ae3e7c01ca37a710bc9eb764
swsong <swsong@gncloud.kr>
2018-05-09 11:45:57 +0900

    벌크 가격가져오기 추가.

--- bulk-collector.js
+++ bulk-collector.js
@@ -1,51 +1,40 @@
-/*jshint esversion: 6 */
 var request = require("request");
 var ccxt = require('ccxt');
 var queue = require("./queue");
 var QUEUE_URL = process.env.QUEUE_URL;
 let limit = 10
-let since = 1491004800000
-exports.handler = (event, context, callback) => {
+let since = 1491004800000 // 2017.4.1
+exchange_id = process.env.EXCHANGE;
 
-    (async () => {
-        let exchange_id = event.exchange
-        let exchange = new (ccxt)[exchange_id] ()
-        let markets = await exchange.loadMarkets ()
-        //console.log(markets)
-        let i = 1
-        for (var symbol in markets) {
-            market = markets[symbol]
-
-            let base = market.info.quoteAsset
-            let coin = market.info.baseAsset
-            let k = 1
-            console.log(i, k, symbol)
-            while(true) {
-                dataList = await exchange.fetchOHLCV (symbol, '1m', since, limit)
-                if (process.env.NODE_ENV == 'dev') {
-                    console.log(exchange_id, coin, base, symbol, dataList);
-                }
-                for (let m =0; m < dataList.length; m++) {
-                    data = dataList[m]
-                    let ts = data[0]
-                    let ohlcv = {
-                        "o": data[1],
-                        "h": data[2],
-                        "l": data[3],
-                        "c": data[4],
-                        "v": data[5]
-                    }
-                    //queue.put(ohlcv, ts, market, coin, base, QUEUE_URL);
-                    //TODO 마지막 시간을 확인하고 다음 루프의 since 시간으로 셋팅한다.
-                    // console.log(ohlcv, ts, market, coin, base, QUEUE_URL);
-                    console.log(ts, coin, base, since, ohlcv);
-                }
-
-                await new Promise(x => setTimeout(x, 1000))
-                k++
+(async () => {
+    let exchange = new (ccxt)[exchange_id] ()
+    let markets = await exchange.loadMarkets ()
+    let i = 1
+    for (let symbol in markets) {
+        market = markets[symbol]
+        let base = market.info.quoteAsset
+        let coin = market.info.baseAsset
+        let k = 1
+        console.log(i, k, exchange_id, symbol)
+        while(true) {
+            console.log(since, symbol, k);
+            dataList = await exchange.fetchOHLCV (symbol, '1m', since, limit)
+            size = dataList.length
+            if(size == 0){
+                console.log('No more data since ', since)
+                break;
             }
-            console.log('--------------------------')
-            i++
+            if (process.env.DRY_RUN != 'true') {
+                queue.bulk_put(dataList, exchange_id, coin, base, QUEUE_URL);
+            }
+
+            await new Promise(x => setTimeout(x, 1000))
+            lastData = dataList[size - 1]
+            // 마지막 시간을 확인하고 다음 루프의 since 시간으로 셋팅한다.
+            since = lastData[0]
+            k++
         }
-    }) ()
-}
+        console.log('--------------------------')
+        i++
+    }
+}) ()
--- queue.js
+++ queue.js
@@ -1,6 +1,6 @@
 var AWS = require('aws-sdk');
 
-exports.put = function(ohlcv, timestamp, market, coin, base, queueUrl) {
+exports.put = function (ohlcv, timestamp, market, coin, base, queueUrl) {
     var messageBody = JSON.stringify(ohlcv);
     // Set the region
     AWS.config.update({
@@ -33,11 +33,70 @@ exports.put = function(ohlcv, timestamp, market, coin, base, queueUrl) {
         QueueUrl: queueUrl
     };
 
-    sqs.sendMessage(params, function(err, data) {
+    sqs.sendMessage(params, function (err, data) {
         if (err) {
             console.log("Error", err);
             // } else {
             //   console.log("Success", data.MessageId);
         }
+    });
+}
+
+exports.bulk_put = function (dataList, exchange, coin, base, queueUrl) {
+
+    // Set the region
+    AWS.config.update({
+        region: 'ap-northeast-2'
+    });
+    var sqs = new AWS.SQS({
+        apiVersion: '2012-11-05'
+    });
+
+    entries = []
+
+    for (let i = 0; i < dataList.length; i++) {
+        data = dataList[i]
+        timestamp = data[0]
+        candle = {
+            'o': data[1],
+            'h': data[2],
+            'l': data[3],
+            'c': data[4],
+            'v': data[5],
+        }
+        let messageBody = JSON.stringify(candle);
+        let entry = {
+            Id: i.toString(),
+            MessageAttributes: {
+                "timestamp": {
+                    DataType: "Number",
+                    StringValue: timestamp.toString()
+                },
+                "market": {
+                    DataType: "String",
+                    StringValue: exchange
+                },
+                "coin": {
+                    DataType: "String",
+                    StringValue: coin
+                },
+                "base": {
+                    DataType: "String",
+                    StringValue: base
+                }
+            },
+            MessageBody: messageBody
+        };
+        entries.push(entry)
+    }
+
+    var params = {
+        Entries: entries,
+        QueueUrl: queueUrl
+    };
+
+    sqs.sendMessageBatch(params, function (err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else console.log(data);           // successful response
     });
 }
