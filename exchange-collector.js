292e47d0ac44e62c9a259ccc6e0bb02ef8b903f6
swsong <swsong@gncloud.kr>
2018-05-09 17:20:14 +0900

    ..

--- exchange-collector.js
+++ exchange-collector.js
@@ -3,27 +3,29 @@ let queue = require("./queue");
 const QUEUE_URL = process.env.QUEUE_URL;
 const maxSize = 3
 exports.handler = (event, context, callback) => {
+    let coin = event.coin
+    let base = event.base
+    let symbol = event.symbol
     let exchange_id = event.exchange;
-    let exchange = new ccxt[exchange_id] ()
+    let exchange = new ccxt[exchange_id]()
     if (exchange.has.fetchOHLCV == true) {
+        let dataList;
         (async () => {
-          let coin = event.coin;
-          let base = event.base;
-          let symbol = event.symbol;
+            dataList = await exchange.fetchOHLCV(symbol, '1m', undefined, maxSize)
+        })()
 
-          dataList = await exchange.fetchOHLCV (symbol, '1m', undefined, maxSize)
-          let data = dataList[dataList.length - 1]
+        let data = dataList[dataList.length - 1]
 
-          if (process.env.NODE_ENV == 'dev') {
-              console.log(exchange_id, ts, symbol, coin, base, ohlcv);
-          }
-          if (process.env.DRY_RUN != 'true') {
-              if (ohlcv) {
-                  queue.put(data, symbol, coin, base, QUEUE_URL);
-              }
-          }
-        }) ()
+        if (process.env.NODE_ENV == 'dev') {
+            console.log(exchange_id, symbol, coin, base, data);
+        }
+        if (process.env.DRY_RUN != 'true') {
+            if (data) {
+                queue.put(data, symbol, coin, base, QUEUE_URL);
+            }
+        }
+
     } else {
-        console.log("[Error] Not support fetchOHLCV for ", exchange_id, 'exchange.has.fetchOHLCV=', exchange.has.fetchOHLCV);
+        console.log(exchange_id, 'exchange not support candle api.')
     }
 }
--- exchange-scheduler.js
+++ exchange-scheduler.js
@@ -1,51 +1,54 @@
-var AWS = require('aws-sdk');
-var ccxt = require('ccxt');
-var lambda = new AWS.Lambda({
+let AWS = require('aws-sdk');
+let ccxt = require('ccxt');
+let lambda = new AWS.Lambda({
     region: 'ap-northeast-2'
 });
-var S3 = new AWS.S3()
-var excnhage = process.env.EXCHANGE.toLowerCase()
-var bucket = process.env.BUCKET
-var s3_prefix = 'static'
-var s3_suffix = '-markets.json'
+let S3 = new AWS.S3()
+let exchange_id = process.env.EXCHANGE.toLowerCase()
+let bucket = process.env.BUCKET
+let s3_prefix = 'static'
+let s3_suffix = '-markets.json'
+let objectKey = `${s3_prefix}/${exchange_id}${s3_suffix}`
 
 exports.handler = (event, context, callback) => {
-  var exchange;
-  var markets;
-  (async () => {
-      console.log('run exchange-scheduler. exchange:', excnhage)
-      exchange = new (ccxt)[excnhage] ()
-      markets = await exchange.load_markets ()
-  })()
-  if (exchange.has.fetchOHLCV == true){
-    let objectKey = `${s3_prefix}/${excnhage}${s3_suffix}`
+    var exchange;
+    var markets;
+    (async () => {
+        console.log('run exchange-scheduler. exchange:', exchange_id)
+        exchange = new (ccxt)[exchange_id]()
+        markets = await exchange.load_markets()
+    })()
+
+    //1. 최신 market symbol 들을 s3 정적호스팅에 업데이트 해준다.
     S3.putObject({
         Bucket: bucket,
         Key: objectKey,
         Body: JSON.stringify(markets),
         ContentType: "application/json"
-      },function (resp) {
+    }, function (resp) {
         console.log(`Successfully uploaded markets. => bucket:${bucket}, objectKey: ${objectKey}`);
     });
 
-    for (var key in markets) {
-        market = markets[key]
-        let attr = {
-            base: market.info.quoteAsset,
-            coin: market.info.baseAsset,
-            symbol: market.info.symbol,
-            exchange: exchange
-        };
-        let functionName = `${excnhage}-collector`
-        console.log('excute lambda function ', functionName, ', attr:', attr)
-        lambda.invoke({
-            FunctionName: functionName,
-            Payload: JSON.stringify(attr)
-        }, function(err, data) {
-            if (err) console.log("err: ", attr.base, attr.coin, err, data);
-        });
+    //2. 캔들정보 함수가 있다면
+    if (exchange.has.fetchOHLCV == true) {
+        for (let symbol in markets) {
+            market = markets[symbol]
+            let attr = {
+                base: market.info.quoteAsset,
+                coin: market.info.baseAsset,
+                symbol: market.symbol,
+                exchange: exchange_id
+            };
+            let functionName = `exchange-collector`
+            console.log('call lambda function ', functionName, ', attr:', attr)
+            lambda.invoke({
+                FunctionName: functionName,
+                Payload: JSON.stringify(attr)
+            }, function (err, data) {
+                if (err) console.log(attr.base, attr.coin, err, err.stack);
+            });
+        }
+    } else {
+        console.log(exchange_id, 'exchange not support candle api.')
     }
-  }else {
-    console.log('Unsupported exchange OHLCV')
-  }
 }
