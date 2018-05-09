6a063735dff60963074d210f61885de5130761b3
joonwoo88 <jwkim@gncloud.kr>
2018-05-09 11:42:47 +0900

    s3 markets upload

--- exchange-scheduler.js
+++ exchange-scheduler.js
@@ -1,16 +1,31 @@
 var AWS = require('aws-sdk');
 var ccxt = require('ccxt');
-
 var lambda = new AWS.Lambda({
     region: 'ap-northeast-2'
 });
+var S3 = new AWS.S3()
+excnhage = process.env.EXCHANGE
+bucket = process.env.BUCKET
+var s3_prefix = 'static'
+var s3_suffix = '-markets.json'
 
 exports.handler = (event, context, callback) => {
   (async () => {
-      let exchange_id = process.env.EXCHANGE
+      let exchange_id = excnhage
       let exchange = new (ccxt)[exchange_id] ()
       let markets = await exchange.load_markets ()
       if (exchange.has.fetchOHLCV == true){
+
+        let objectKey = `${s3_prefix}/${exchange_id.toLowerCase()}${s3_suffix}`
+        S3.putObject({
+            Bucket: bucket,
+            Key: objectKey,
+            Body: JSON.stringify(markets),
+            ContentType: "application/json"
+          },function (resp) {
+            console.log(`Successfully uploaded markets. => bucket:${bucket}, objectKey: ${objectKey}`);
+        });
+
         for (var key in markets) {
             market = markets[key]
             let attr = {
