27792b20c03c8c4933a7ee6e3c1de4a65ad549fd
swsong <swsong@gncloud.kr>
2018-05-11 15:47:01 +0900

    bulk

--- bulk/bulk-lambda-multiprocess-run.sh
+++ bulk/bulk-lambda-multiprocess-run.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 trap '' 1
 export IFS=","
-SYMBOLS="ETH/BTC,BTC/USDT"
-batchSize=10
+args=("$@")
 COUNTER=1
+ID=${args[0]}
+SYMBOLS=${args[1]}
+echo '>>>' ID: $ID, SYMBOLS: $SYMBOLS
 for symbol in $SYMBOLS; do
-  echo "$COUNTER >> $symbol"
-  #node bulk-lambda-scheduler.js $SYMBOLS >> system-lambda-$COUNTER.log 2>&1 &
-  sleep 0.3
+  echo ">> $COUNTER $symbol"
+  node bulk-worker-run.js $ID $SYMBOLS >> system-lambda.log 2>&1
   COUNTER=$[$COUNTER +1]
 done
 
