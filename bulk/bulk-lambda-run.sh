12a9c79f6f6b6d0e8138ec67a0909060b1dcf790
swsong <swsong@gncloud.kr>
2018-05-14 07:45:34 +0900

    정리

--- bulk/bulk-lambda-run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-trap '' 1
-node bulk-lambda-scheduler.js >> system-lambda.log 2>&1 &
--- bulk/bulk-run.sh
+++
@@ -1,9 +0,0 @@
-export QUEUE_URL=https://sqs.ap-northeast-2.amazonaws.com/868448630378/bulk-price
-export EXCHANGE=binance
-export LIMIT=500
-#since = 1491004800000 // 2017.4.1
-#since = 1525844400000 //2018.5.9 14:40:00
-export SINCE_TIME=1491004800000
-
-trap '' 1
-node bulk-collector.js >> system.log 2>&1 &
